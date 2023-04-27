720c1b22b670254cb8268c28e968665c005c0ff5
nooty <annsk5342@gmail.com>
2023-04-28 01:10:19 +0900

    구글로그인 에러와 aws-ec2-mysql 에러발생해서 고치는중

--- app/src/main/java/com/example/yachtdicev2/Select_Activity.java
+++ app/src/main/java/com/example/yachtdicev2/Select_Activity.java
@@ -2,13 +2,69 @@ package com.example.yachtdicev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Select_Activity extends AppCompatActivity {
+
+    String loginUserNickname;
+    String TAG = "Select_Activity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        TextView userNickname = (TextView) findViewById(R.id.User_nickname);
+        Button vsUser_bt = (Button) findViewById(R.id.VS_USER);
+        Button vsMulti_bt = (Button) findViewById(R.id.VS_MULTI_USER);
+        Button vsCPU_bt = (Button) findViewById(R.id.VS_CPU);
+        Button ranking_bt = (Button) findViewById(R.id.RANKING);
+
+        Intent selectIntent = getIntent();
+        loginUserNickname = selectIntent.getStringExtra("loginNickname");
+        Log.e(TAG,"get 값 : " + loginUserNickname);
+
+        userNickname.setText(loginUserNickname);
+
+
+        userNickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Select_Activity.this,Mypage_Activity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        vsCPU_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Select_Activity.this,Ingame_Activity.class);
+                intent.putExtra("loginUserNickName",loginUserNickname);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
